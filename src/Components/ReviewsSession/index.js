d5d7924c09617ff9794f2cea55856bc5fdc8a741
Vania Tavares <vntavares56@gmail.com>
2022-01-19 18:49:17 +0000

    comments working

--- src/Components/ReviewsSession/index.js
+++ src/Components/ReviewsSession/index.js
@@ -2,18 +2,22 @@ import React, { useState } from "react";
 import divisions from "../../Assets/hackaton.json";
 
 const ReviewsSession = () => {
-  const [reviews, setReviews] = useState([
-    divisions.divisions.office[0].comments,
-  ]);
+  const [reviews, setReviews] = useState(
+    divisions.divisions.office[0].comments
+  );
   console.log(reviews);
   return (
     <div className="reviews-holder" style={{ display: "block" }}>
       {reviews.map((review, index) => (
         <div key={review.user + index}>
           <h4>
-            {review.date} {review.user}
+            {review.date || "Hello"} {review.user}
           </h4>
-          <p>{review.text}</p>
+          <p>
+            {review.text ||
+              "Lorem, ipsum dolor sit amet consectetur adipisicing elit."}
+          </p>
+          <br />
         </div>
       ))}
     </div>
