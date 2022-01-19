bf8e83024db95787e03887b4f9d41d929f1f441a
Diogo <hyperdg12@gmail.com>
2022-01-19 16:23:52 +0000

    did image carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Slider from "./Components/Slider";
 
 function App() {
   return (
-  <div>
-    <Header />
-  </div>
-  )
+    <div>
+      <Header />
+      <Slider />
+    </div>
+  );
 }
 
 export default App;
