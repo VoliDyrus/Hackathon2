5e19ccfbb04383a1d2ff34b1a5eeb34388c507d2
Vania Tavares <vntavares56@gmail.com>
2022-01-20 17:33:00 +0000

    added more divisions

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,11 +2,22 @@ import { useState } from "react";
 import "./header.css";
 import headerImg from "./image1.png";
 
-function Header() {
-  const [toggleState, setToggleState] = useState(1);
+const divisions = [
+  "Office",
+  "Kitchen",
+  "Bathroom",
+  "Bedroom",
+  "Living Room",
+  "Dining Room",
+  "Garden",
+  "Balcony",
+];
 
-  const toggleTab = (index) => {
-    setToggleState(index);
+function Header() {
+  const [toggleState, setToggleState] = useState("Office");
+
+  const toggleTab = (name) => {
+    setToggleState(name);
   };
 
   return (
@@ -14,64 +25,33 @@ function Header() {
       <img src={headerImg} alt="header" width="100%" />
       <div className="container">
         <div className="bloc-tabs">
-          <button
-            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(1)}
-          >
-            Office
-          </button>
-          <button
-            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(2)}
-          >
-            Kitchen
-          </button>
+          {divisions.map((division) => (
+            <button
+              key={division}
+              className={toggleState === division ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(division)}
+            >
+              {division}
+            </button>
+          ))}
         </div>
 
         <div className="content-tabs">
-          <div
-            className={
-              toggleState === 1 ? "content  active-content" : "content"
-            }
-          >
-            <h2>Office Project</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-              praesentium incidunt quia aspernatur quasi quidem facilis quo
-              nihil vel voluptatum?
-            </p>
-          </div>
-
-          <div
-            className={
-              toggleState === 2 ? "content  active-content" : "content"
-            }
-          >
-            <h2>Kitchen Project</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-              voluptatum qui adipisci.
-            </p>
-          </div>
-
-          <div
-            className={
-              toggleState === 3 ? "content  active-content" : "content"
-            }
-          >
-            <h2>Content 3</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
-              nostrum rerum laudantium totam unde adipisci incidunt modi alias!
-              Accusamus in quia odit aspernatur provident et ad vel distinctio
-              recusandae totam quidem repudiandae omnis veritatis nostrum
-              laboriosam architecto optio rem, dignissimos voluptatum beatae
-              aperiam voluptatem atque. Beatae rerum dolores sunt.
-            </p>
-          </div>
+          {divisions.map((category) => (
+            <div
+              className={
+                toggleState === category ? "content  active-content" : "content"
+              }
+            >
+              <h2>{category} Project</h2>
+              <hr />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati praesentium incidunt quia aspernatur quasi quidem
+                facilis quo nihil vel voluptatum?
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </>
