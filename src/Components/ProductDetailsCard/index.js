550a61198718f3473b76e0f5af45d391fca672e5
Vania Tavares <vntavares56@gmail.com>
2022-01-20 14:39:24 +0000

    adding total

--- src/Components/ProductDetailsCard/index.js
+++ src/Components/ProductDetailsCard/index.js
@@ -1,43 +1,33 @@
-import React, {useState} from "react";
-import {plus, minus} from "../../Assets/index";
+import React from "react";
+import { plus, minus } from "../../Assets/index";
 import "./index.css";
 
-const ProductDetailsCard = ({ product, products }) => {
-
-  const[quantity, setQuantity] = useState(1)
-  const[totalPrice, setTotalPrice] = useState(quantity*product.price)
-  const productTotal = {...product, quantity, totalPrice}
-
-  const increaseItemQtt = () => {
-    setQuantity(quantity +1)
-    setTotalPrice((quantity+1)*product.price)
-  }
-
-  const decreaseItemQtt = () => {
-    setQuantity(quantity - 1)
-    setTotalPrice((quantity-1)*product.price)
-  }  
-
+const ProductDetailsCard = ({ product, changeQuantity, removeItem }) => {
   return (
     <div className="card-holder">
       <div className="card">
         <img src={product.image} alt={product.name} />
         <br />
-        <span>{product.name}</span>&nbsp;<span>Unit Price: €{product.price}</span>
-        
+        <span>{product.name}</span>&nbsp;
+        <span>Unit Price: €{product.price}</span>
         <div className="button-container">
-        <button className='amount-btn' onClick={decreaseItemQtt}>
-            <img src={minus} alt=""/>
+          <button
+            className="amount-btn"
+            onClick={() => changeQuantity(product.name, "subtract")}
+          >
+            <img src={minus} alt="" />
           </button>
-            {quantity}
-            <button className='amount-btn' onClick={increaseItemQtt}>
-            <img src={plus} alt=""/>
+          {product.quantity}
+          <button
+            className="amount-btn"
+            onClick={() => changeQuantity(product.name, "add")}
+          >
+            <img src={plus} alt="" />
           </button>
-          
         </div>
-        {totalPrice}
-
-        <button className="remove-btn">Remove</button>
+        <button className="remove-btn" onClick={() => removeItem(product.name)}>
+          Remove
+        </button>
       </div>
     </div>
   );
--- src/Components/ProductList/index.js
+++ src/Components/ProductList/index.js
@@ -1,20 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProductDetailsCard from "../ProductDetailsCard";
 import divisions from "../../Assets/hackaton.json";
+const original = divisions.divisions.office[0].components;
 
 const ProductList = () => {
   const [toggleState, setToggleState] = useState("Furniture");
-  const [tabs, setTabs] = useState(
-    Object.keys(divisions.divisions.office[0].components)
-  );
-  const [products, setProducts] = useState([
-    divisions.divisions.office[0].components,
-  ]);
-  console.log(products)
+  const [tabs, setTabs] = useState(original.types);
+  const [products, setProducts] = useState(original.products);
+  const [basePrice, setBasePrice] = useState(0);
+  const [finalPrice, setFinalPrice] = useState(0);
+
   const toggleTab = (category) => {
     setToggleState(category);
   };
 
+  const totalAmount = (array) => {
+    const reducer = (previousValue, currentValue) =>
+      previousValue + currentValue.price * currentValue.quantity;
+    return array.reduce(reducer, 0).toFixed(2);
+  };
+
+  const changeQuantity = (name, operation) => {
+    setProducts((prev) => {
+      const newQuantity = [...prev];
+      const indexToUpdate = newQuantity.findIndex((elt) => elt.name === name);
+      if (operation === "add") {
+        newQuantity[indexToUpdate].quantity =
+          newQuantity[indexToUpdate].quantity + 1;
+      } else {
+        newQuantity[indexToUpdate].quantity =
+          newQuantity[indexToUpdate].quantity - 1;
+      }
+      return newQuantity;
+    });
+  };
+
+  const removeItem = (name) => {
+    setProducts((prev) => {
+      return prev.filter((elt) => elt.name !== name);
+    });
+  };
+
+  useEffect(() => {
+    setBasePrice(totalAmount(products));
+    setFinalPrice(totalAmount(products));
+  }, []);
+
+  useEffect(() => {
+    setFinalPrice(totalAmount(products));
+  }, [products]);
+
+  console.log(products, basePrice, finalPrice);
   return (
     <div className="product-list">
       <div className="bloc-tabs">
@@ -29,18 +65,24 @@ const ProductList = () => {
       </div>
 
       <div className="content-tabs">
-        {tabs.map((category) => (
-          <div
-            className={
-              toggleState === category ? "content  active-content" : "content"
-            }
-          >
-            {products[0][category].map((elt) => (
-              <ProductDetailsCard product={elt} products={products} />
-            ))}
-            
-          </div>
-        ))}
+        {products.length &&
+          tabs.map((category) => (
+            <div
+              className={
+                toggleState === category ? "content  active-content" : "content"
+              }
+            >
+              {products
+                .filter((product) => product.type === category)
+                .map((elt) => (
+                  <ProductDetailsCard
+                    product={elt}
+                    changeQuantity={changeQuantity}
+                    removeItem={removeItem}
+                  />
+                ))}
+            </div>
+          ))}
       </div>
     </div>
   );
